105b64181bd12127e789f6483d364fec7083f964
ay286677ty <ay286677ty@gmail.com>
2025-12-29 13:43:29 +0530

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+const navLinks = [
+  { label: "Store" },
+  { label: "Mac" },
+  { label: "Vision" },
+  { label: "iPhone" },
+  { label: "AirPods" },
+  { label: "Watch" },
+];
+export { navLinks };
