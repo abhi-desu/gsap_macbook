07be6084814a4f4e5a0cef7c878f27718aaf2e5e
ay286677ty <ay286677ty@gmail.com>
2025-12-29 14:27:08 +0530

    Implementation of hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 const App = () => {
-  return <Navbar />;
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+    </main>
+  );
 };
 
 export default App;
